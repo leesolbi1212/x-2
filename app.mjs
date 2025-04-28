32f0e3dfbd83d14258d6766a0d049567f3380e9f
이솔비 <dmddnjs77@gmail.com>
2025-04-28 15:59:01 +0900

    새로운 마음으로 새로 시작!!

---
+++ app.mjs
@@ -0,0 +1,17 @@
+import express from "express";
+import postsRouter from "./router/posts.mjs";
+import authRouter from "./router/auth.mjs";
+import { config } from "./config.mjs";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/posts", postsRouter);
+app.use("/auth", authRouter);
+
+app.use((req, res, next) => {
+  res.sendStatus(404);
+});
+
+app.listen(config.host.port);
