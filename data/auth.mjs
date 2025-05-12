461f6b911bc4006888736c491a03da3d0456609c
이솔비 <dmddnjs77@gmail.com>
2025-05-12 11:34:51 +0900

    Mongoose 적용

--- data/auth.mjs
+++ data/auth.mjs
@@ -1,29 +1,47 @@
-import MongoDb from "mongodb";
-import { getUsers } from "../db/database.mjs";
-const ObjectID = MongoDb.ObjectId;
+// import MongoDb from "mongodb";
+// import { getUsers } from "../db/database.mjs";
+import Mongoose from "mongoose";
+import { useVirtualId } from "../db/database.mjs";
 
+// 테이블을 만드는 구조와 비슷함
+const userSchema = new Mongoose.Schema(
+  {
+    userid: { type: String, require: true },
+    name: { type: String, require: true },
+    email: { type: String, require: true },
+    password: { type: String, require: true },
+    url: String,
+  },
+  { versionKey: false } //몽구스는 데이터를 넣으면 버전키가 자동으로 들어가기 때문에 안 넣기로~
+);
+
+useVirtualId(userSchema);
+
+const User = Mongoose.model("User", userSchema);
+// 컬렉션 이름에 s가 자동으로 붙어서 Users가 만들어진다. 그 컬렉션의 틀이 userSchema가 된다.
+
+// const ObjectID = MongoDb.ObjectId;
+
+// 사용자 만들기
 export async function createUser(user) {
-  return getUsers()
-    .insertOne(user)
-    .then((result) => result.insertedId.toString());
+  return new User(user).save().then((data) => data.id);
 }
 
-export async function login(userid, password) {
-  const user = users.find(
-    (user) => user.userid === userid && user.password === password
-  );
-  return user;
-}
+// export async function login(userid, password) {
+//   const user = users.find(
+//     (user) => user.userid === userid && user.password === password
+//   );
+//   return user;
+// }
 
+// 로그인 할 때 아이디가 있는지 체크하는 함수
 export async function findByUserid(userid) {
-  return getUsers().find({ userid }).next().then(mapOptionalUser);
+  return User.findOne({ userid }); //하나를 찾는 메서드(findOne)
 }
 
+// 아이디로 포스트, 나의 정보를 찾는 함수
 export async function findByid(id) {
-  return getUsers()
-    .find({ _id: new ObjectID(id) })
-    .next()
-    .then(mapOptionalUser);
+  return User.findById(id);
 }
 
 function mapOptionalUser(user) {
--- db/database.mjs
+++ db/database.mjs
@@ -1,14 +1,40 @@
+/*
+   몽고DB의 장점이자 단점 : 스키마가 없음! 골격이나 제한 사항이 없다보니까 실수해도 잘 넘어감 ㅠㅠ 
+  Mongoosee 
+  - 몽고DB + Node.js용 ORM (Object-Relational Mapping) sql 언어를 쓰지 않아도 자바언어로 접근할 수 있음. 데이터 베이스를 객체로 봄. 
+  - 스키마를 정의
+  - 입력, 수정, 조회, 삭제 모두 안정적이고 코드를 간결하게 작성 
+  - 
+ 
+*/
+
 import { config } from "../config.mjs";
-import MongoDb from "mongodb";
+
+import Mongoose from "mongoose";
 
 let db;
+
+export async function connectDB() {
+  return Mongoose.connect(config.db.host);
+}
+
+export function useVirtualId(Schema) {
+  Schema.virtual("id").get(function () {
+    return this._id.toString();
+  });
+  Schema.set("toJSON", { virtual: true });
+  Schema.set("toObject", { virtual: true });
+}
+
+/* 원래는 몽고디비와 직접 연결하는 임포트를 썼었음
+
+import MongoDb from "mongodb"; 
 
 export async function connectDB() {
   return MongoDb.MongoClient.connect(config.db.host).then((client) => {
     db = client.db();
     // console.log(db);
   });
-}
 
 export function getUsers() {
   return db.collection("users");
@@ -16,4 +42,4 @@ export function getUsers() {
 
 export function getPosts() {
   return db.collection("posts");
-}
+} */
